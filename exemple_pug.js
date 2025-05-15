c0841d36295ed7b3cc868c76ce87eb16f05b4eb9
Brasero <brandonricci4@gmail.com>
2025-05-15 13:24:23 +0200

    proposition de correction

--- exemple_pug.js
+++ exemple_pug.js
@@ -1,73 +1,27 @@
 import pug from "pug"
 import path from 'node:path'
-import http from 'node:http'
 
 const dirname = import.meta.dirname
 const viewPath = path.join(dirname, "view")
 
-// const template = `
-// if age >= 18
-// 	h1 Accès autorisé
-// else
-// 	h1 Accès refusée
-// `;
+const loggedUser = {
+	name: {
+		first: 'Jean',
+		last: 'Dupont',
+	},
+	age: 36,
+	birthdate: new Date('1986-04-18'),
+	location: {
+		zipcode: '77420',
+		city: 'Champs-sur-Marne',
+	},
+	isAdmin: true
+};
 
-// const compileTemplate = pug.compile(template)
-// const result = compileTemplate({ age: 17 })
-
-// pug.render(template, {age: 19}, (err, data) => {
+// pug.renderFile(path.join(viewPath, "user.pug"), {pretty: true, user: loggedUser}, (err, data) => {
 // 	if (err) throw err
 // 	console.log(data)
 // })
 
-// const compileTemplate = pug.compileFile(path.join(viewPath, "exemple.pug"));
-// console.log(compileTemplate({age: 18}))
-
-// pug.renderFile(path.join(viewPath, "exemple.pug"), {age: 12}, (err, data) => {
-// 	if (err) throw err
-// 	console.log(data)
-// })
-
-// const server = http.createServer((req, res) => {
-// 	const url = req.url.replace("/", "")
-// 	const user = {
-// 		isAdmin: true
-// 	}
-// 	if (url === "") {
-// 		user.isAdmin = false
-// 	}
-//
-// 	pug.renderFile(path.join(viewPath, "admin.pug"), {user}, (err, data) => {
-// 		if (err) {
-// 			res.writeHead(500, {
-// 				'content-type': "text/plain"
-// 			})
-// 			res.end("Une erreur s'est produite lors de la compilation")
-// 			return
-// 		}
-// 		res.writeHead(200, {
-// 			"content-type": "text/html"
-// 		})
-// 		res.end(data)
-// 	})
-// })
-//
-// server.listen(8000, "localhost", () => {
-// 	console.log("server running")
-// })
-
-// pug.renderFile(path.join(viewPath, "nav.pug"), {}, (err, data) => {
-// 	if (err) throw err
-// 	console.log(data)
-// })
-
-const html = `<div>Hello</div>`
-
-const compile = pug.compileFile(path.join(viewPath, "nav.pug"), {pretty: true})
-
-console.log(compile({
-	name: "Paul",
-	age: 32,
-	html,
-	color: "toto"
-}))
+const compile = pug.compileFile(path.join(viewPath, "user.pug"), {pretty: true})
+console.log(compile({user: loggedUser}))
