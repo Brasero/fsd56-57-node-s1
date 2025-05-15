92dd28e1b811cd8ccced2227cd8cd8dad3b0dac2
Brasero <brandonricci4@gmail.com>
2025-05-15 15:55:58 +0200

    proposition de correction

--- server.js
+++ server.js
@@ -1,65 +1,21 @@
-import http from "node:http";
-import dotenv from "dotenv";
-import path from "node:path";
-import pug from "pug";
-import fs from "node:fs";
-
-dotenv.config();
-
-const {HOST, PORT} = process.env
+import pug from 'pug'
+import path from "node:path"
 
 const dirname = import.meta.dirname
+
 const viewPath = path.join(dirname, "view")
-const assetsPath = path.join(dirname, "assets")
 
-const server = http.createServer((req, res) => {
-	
-	const url = req.url.replace("/", "")
-	
-	if (url === "favicon.ico") {
-		res.writeHead(200, {
-			"content-type": "image/x-icon"
-		})
-		res.end()
-		return
-	}
-	
-	if (url.startsWith("style")) {
-		const stylesheetName = url.split("/").pop()
-		const stylesheet = fs.readFileSync(path.join(assetsPath, stylesheetName))
-		
-		res.writeHead(200, {
-			"content-type": "text/css"
-		})
-		res.end(stylesheet)
-		return
-	}
-	
-	if (url === "" ) {
-		res.writeHead(200, {
-			"content-type": "text/html"
-		})
-		
-		pug.renderFile(path.join(viewPath, "home.pug"), {}, (err, data) => {
-			if (err) throw err;
-			res.end(data)
-		})
-		return
-	}
-	
-	if (url === "user") {
-		res.writeHead(200, {
-			"content-type": "text/html"
-		})
-		
-		pug.renderFile(path.join(viewPath, "user.pug"), {}, (err, data) => {
-			if (err) throw err;
-			res.end(data)
-		})
-	}
-	
-})
+const menuItems = [
+	{ path: '/', title: 'Home', isActive: true },
+	{ path: '/about-me', title: 'About', isActive: false },
+	{ path: '/references', title: 'References', isActive: false },
+	{ path: '/contact-me', title: 'Contact', isActive: false },
+];
 
-server.listen(PORT, HOST, () => {
-	console.log("running")
+pug.renderFile(path.join(viewPath, "home.pug"), {
+	pretty: true,
+	menuItems
+}, (err, data) => {
+	if (err) throw err
+	console.log(data)
 })
