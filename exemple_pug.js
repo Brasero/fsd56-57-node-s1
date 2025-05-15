9e9d380658a33380732a3895f4652d73488e968a
Brasero <brandonricci4@gmail.com>
2025-05-15 11:22:38 +0200

    presentation de la notation PUG

--- exemple_pug.js
+++ exemple_pug.js
@@ -1,15 +1,16 @@
 import pug from "pug"
 import path from 'node:path'
+import http from 'node:http'
 
 const dirname = import.meta.dirname
 const viewPath = path.join(dirname, "view")
 
-const template = `
-if age >= 18
-	h1 Accès autorisé
-else
-	h1 Accès refusée
-`;
+// const template = `
+// if age >= 18
+// 	h1 Accès autorisé
+// else
+// 	h1 Accès refusée
+// `;
 
 // const compileTemplate = pug.compile(template)
 // const result = compileTemplate({ age: 17 })
@@ -22,7 +23,51 @@ else
 // const compileTemplate = pug.compileFile(path.join(viewPath, "exemple.pug"));
 // console.log(compileTemplate({age: 18}))
 
-pug.renderFile(path.join(viewPath, "exemple.pug"), {age: 12}, (err, data) => {
-	if (err) throw err
-	console.log(data)
-})
+// pug.renderFile(path.join(viewPath, "exemple.pug"), {age: 12}, (err, data) => {
+// 	if (err) throw err
+// 	console.log(data)
+// })
+
+// const server = http.createServer((req, res) => {
+// 	const url = req.url.replace("/", "")
+// 	const user = {
+// 		isAdmin: true
+// 	}
+// 	if (url === "") {
+// 		user.isAdmin = false
+// 	}
+//
+// 	pug.renderFile(path.join(viewPath, "admin.pug"), {user}, (err, data) => {
+// 		if (err) {
+// 			res.writeHead(500, {
+// 				'content-type': "text/plain"
+// 			})
+// 			res.end("Une erreur s'est produite lors de la compilation")
+// 			return
+// 		}
+// 		res.writeHead(200, {
+// 			"content-type": "text/html"
+// 		})
+// 		res.end(data)
+// 	})
+// })
+//
+// server.listen(8000, "localhost", () => {
+// 	console.log("server running")
+// })
+
+// pug.renderFile(path.join(viewPath, "nav.pug"), {}, (err, data) => {
+// 	if (err) throw err
+// 	console.log(data)
+// })
+
+const html = `<div>Hello</div>`
+
+const compile = pug.compileFile(path.join(viewPath, "nav.pug"), {pretty: true})
+
+console.log(compile({
+	name: "Paul",
+	age: 32,
+	html,
+	color: "toto"
+}))
